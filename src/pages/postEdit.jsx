258a2a8743b62d22eac5a7bcebdeca3c925f700d
Dino0204 <s24020@gsm.hs.kr>
2024-12-23 11:39:32 +0900

    💄 Update: postEdit css

--- src/pages/postEdit.jsx
+++ src/pages/postEdit.jsx
@@ -126,109 +126,105 @@ export default function PostEdit({ postId }) {
   };
 
   return (
-    <div className="flex flex-col items-center w-full">
-      <Header />
+    <div className="flex flex-col items-center w-full h-max">
+      {/* <Header /> */}
       {alertPopUp && <AlertPopUp />}
-      <div className="flex flex-col mt-[60px] w-[600px] p-[36px]">
-        {/*이미지*/}
-        <div className="flex space-x-2 overflow-x-auto whitespace-nowrap scroll-smooth no-scrollbar">
-          <div
-            className="flex items-center justify-center 
+      <div className="flex flex-col w-[37.5rem] h-max p-[3.75rem] py-9 border-x-2 items-center">
+        {/*게시물*/}
+        <div className="flex flex-col w-full gap-14">
+          {/*이미지*/}
+          <div className="flex w-full gap-[10px] overflow-x-auto whitespace-nowrap scroll-smooth no-scrollbar">
+            <div
+              className="flex items-center justify-center 
             border border-gray-400 rounded-lg select-none
             w-[150px] h-[150px] flex-shrink-0"
-          >
-            <input
-              type="file"
-              id="imgUpload"
-              className="hidden"
-              ref={imgRef}
-              onChange={addImage}
-            />
-            <label
-              htmlFor="imgUpload"
-              className="flex items-center justify-center w-full h-full cursor-pointer"
             >
-              <img src={Camera} alt="camera" />
-            </label>
-          </div>
-
-          {imgFiles.map((imgFile, index) => (
-            <div key={index} className="relative">
-              <ImageCard url={URL.createObjectURL(imgFile)} />
-              <button
-                className="absolute p-1 bg-[rgba(255,255,255,0.8)] rounded-full top-2 right-1"
-                onClick={() => deleteImage(index)}
+              <input
+                type="file"
+                id="imgUpload"
+                className="hidden"
+                ref={imgRef}
+                onChange={addImage}
+              />
+              <label
+                htmlFor="imgUpload"
+                className="flex items-center justify-center w-full h-full cursor-pointer"
               >
-                <img src={TrashCan} alt="trash" />
-              </button>
+                <img src={Camera} alt="camera" />
+              </label>
             </div>
-          ))}
-        </div>
-
-        <div className="bg-gray-400 w-full h-[1px] my-5"></div>
-
-        {/*게시물*/}
-        <div className="flex flex-col gap-4">
+            {imgFiles.map((imgFile, index) => (
+              <div key={index} className="relative">
+                <ImageCard url={URL.createObjectURL(imgFile)} />
+                <button
+                  className="absolute p-1 bg-[rgba(255,255,255,0.8)] rounded-full top-2 right-1"
+                  onClick={() => deleteImage(index)}
+                >
+                  <img src={TrashCan} alt="trash" />
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="bg-gray-400 w-full h-[1px] my-5"></div>
           <input
             type="text"
             placeholder="제목을 입력해주세요 (최대20글자)"
             maxLength="20"
-            className="text-4xl font-semibold border-none outline-none"
+            className="text-[2.5rem] font-semibold border-none outline-none"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
           <input
             type="text"
             placeholder="사례금을 입력하세요(최대 100만원)"
-            className="border-none outline-none"
+            className="text-2xl border-none outline-none"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
           />
-
           <textarea
-            className="border-none outline-none resize-none"
+            className="text-xl border-none outline-none resize-none"
             placeholder="설명을 입력하세요(최대 200글자)"
             maxLength="200"
             rows="5"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
+          {/*드롭다운*/}
+          <div className="flex items-center justify-center w-full">
+            <SelectSmall
+              svg={LostItem}
+              title="분실물"
+              itemArray={lostItems}
+              onChange={setSelectedLostItem}
+              selected={selectedLostItem}
+            />
+            <SelectSmall
+              svg={Gwan}
+              title="관"
+              itemArray={gwans}
+              onChange={setSelectedGwan}
+              selected={selectedGwan}
+            />
+            <SelectSmall
+              svg={Floor}
+              title="층"
+              itemArray={
+                selectedGwan === "기숙사" ? floors : floors.slice(0, 4)
+              }
+              onChange={setSelectedFloor}
+              selected={selectedFloor}
+            />
+          </div>
         </div>
-
-        {/*드롭다운*/}
-        <div className="flex items-center justify-center w-full mb-5">
-          <SelectSmall
-            svg={LostItem}
-            title="분실물"
-            itemArray={lostItems}
-            onChange={setSelectedLostItem}
-            selected={selectedLostItem}
-          />
-          <SelectSmall
-            svg={Gwan}
-            title="관"
-            itemArray={gwans}
-            onChange={setSelectedGwan}
-            selected={selectedGwan}
-          />
-          <SelectSmall
-            svg={Floor}
-            title="층"
-            itemArray={selectedGwan === "기숙사" ? floors : floors.slice(0, 4)}
-            onChange={setSelectedFloor}
-            selected={selectedFloor}
-          />
-        </div>
-
         {/* 제출 */}
         <button
-          className={`px-[36px] py-[12px] bg-primary-primary rounded-[40px] text-[white] ml-auto ${
+          className={`px-4 py-3 bg-primary-primary rounded-3xl text-[white] ${
             isFormValid ? "bg-opacity-100" : "bg-opacity-50"
           }`}
           disabled={!isFormValid}
           onClick={updatePost}
         >
-          수정
+          추가하기
         </button>
       </div>
     </div>
